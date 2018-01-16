60d64849d5dfb752a67a5e9fec0dee4b5b077332
ClaytonNighthawk <nighthawk1029tv@gmail.com>
2018-01-15 16:14:23 -0800

    adding preliminary ideas about how the classes should be set up

---
+++ src/edu/uoregon/scrumthing/AddressBook.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class AddressBook {
+	private HashMap<String, Contact> contacts = new HashMap<>();
+	private ArrayList <Contact> sortedContactList = new ArrayList<>();
+}
---
+++ src/edu/uoregon/scrumthing/Contact.java
@@ -0,0 +1,20 @@
+public class Contact {
+    public String firstName;
+    public String lastName;
+    public String address;
+    public String city;
+    public String state;
+    public String zip;
+    public String email;
+    public String phoneNumber;
+
+    public Contact(String _firstName, String _lastName, String _address, String _city, String _state, String _zip, String _email, String _phoneNumber) {
+        firstName = _firstName;
+        lastName = _lastName;
+        address = _address;
+        city = _city;
+        state = _state;
+        zip = _zip;
+        email = _email;
+        phoneNumber = _phoneNumber;
+    }
