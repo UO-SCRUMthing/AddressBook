4ee344b224425bdecae6f2ca4b97d4547da2d381
Clayton Nighthawk <nighthawk1029tv@gmail.com>
2018-01-17 17:09:00 -0800

    adding classes that extend the abstact classes

--- src/edu/uoregon/scrumthing/AddressBook.java
+++ src/edu/uoregon/scrumthing/AddressBook.java
@@ -1,8 +1,41 @@
+package edu.uoregon.scrumthing;
+
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class AddressBook {
+public class AddressBook extends EntryContainer {
 	private HashMap<String, Contact> contacts = new HashMap<>();
 	private ArrayList <Contact> sortedContactList = new ArrayList<>();
+	
+	@Override
+	public boolean isChanged() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public void addEntry(Entry entry) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void removeEntry(Entry entry) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean sortBy(String field) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public Entry getEntry(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
--- src/edu/uoregon/scrumthing/Contact.java
+++ src/edu/uoregon/scrumthing/Contact.java
@@ -1,20 +1,40 @@
-public class Contact {
-    public String firstName;
-    public String lastName;
-    public String address;
-    public String city;
-    public String state;
-    public String zip;
-    public String email;
-    public String phoneNumber;
+package edu.uoregon.scrumthing;
 
-    public Contact(String _firstName, String _lastName, String _address, String _city, String _state, String _zip, String _email, String _phoneNumber) {
-        firstName = _firstName;
-        lastName = _lastName;
-        address = _address;
-        city = _city;
-        state = _state;
-        zip = _zip;
-        email = _email;
-        phoneNumber = _phoneNumber;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Contact extends Entry {
+    List<SimpleEntry<String, String>> detailList = new ArrayList<SimpleEntry<String, String>>();    
+
+    public Contact(String _firstName, String _lastName, String _address, String _city, String _state, String _zip, String _email, String _phoneNumber, List<SimpleEntry<String, String>> _userDefined) {
+        detailList.add(new SimpleEntry<String, String>("firstName", _firstName));
+        detailList.add(new SimpleEntry<String, String>("lastName", _lastName));
+        detailList.add(new SimpleEntry<String, String>("address", _address));
+        detailList.add(new SimpleEntry<String, String>("city", _city));
+        detailList.add(new SimpleEntry<String, String>("state", _state));
+        detailList.add(new SimpleEntry<String, String>("zip", _zip));
+        detailList.add(new SimpleEntry<String, String>("email", _email));
+        detailList.add(new SimpleEntry<String, String>("phoneNumber", _phoneNumber));
+        detailList.addAll(_userDefined);
     }
+
+	@Override
+	public String getName() {
+		return detailList.get(0).getValue() + " " + detailList.get(1).getValue();
+	}
+
+	@Override
+	public List<SimpleEntry<String, String>> getDetailList() {
+		return detailList;
+	}
+
+	@Override
+	public boolean updateDetail(HashMap<String, String> detailMap) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+    
+    
+}
